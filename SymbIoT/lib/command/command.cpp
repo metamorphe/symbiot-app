eb82d31717acacb79500429ce40944edf82ff5bd
Jasper O'Leary <jhaazpr@gmail.com>
2015-04-04 19:28:41 -0700

    Add command library
    The command library allows a scheduler to send the actuator state
    of a specified node address to a certain value, without having
    to provide predefined blink_m scripts. See command.h for more
    information.

---
+++ SymbIoT/lib/command/command.cpp
@@ -0,0 +1,43 @@
+#include "command.h"
+
+void
+command_set_color (uint16_t to_node, uint16_t from_node,
+                   uint8_t red_val, uint8_t green_val,
+                   uint8_t blue_val)
+{
+  blinkm_script_line set_line = { 1, { 'n', red_val, blue_val, green_val} };
+  send (to_node, from_node, &set_line, 1);
+}
+
+void
+command_turn_on (uint16_t to_node, uint16_t from_node)
+{
+  command_set_color (to_node, from_node, 0xff, 0xff, 0xff);
+}
+
+void
+command_turn_off (uint16_t to_node, uint16_t from_node)
+{
+  command_set_color (to_node, from_node, 0x00, 0x00, 0x00);
+}
+
+void
+command_fade_to_color (uint16_t to_node, uint16_t from_node,
+                   uint8_t red_val, uint8_t green_val,
+                   uint8_t blue_val)
+{
+  blinkm_script_line set_line = { 1, { 'c', red_val, blue_val, green_val} };
+  send (to_node, from_node, &set_line, 1);
+}
+
+void
+command_fade_on (uint16_t to_node, uint16_t from_node)
+{
+  command_fade_to_color (to_node, from_node, 0xff, 0xff, 0xff);
+}
+
+void
+command_fade_off (uint16_t to_node, uint16_t from_node)
+{
+  command_fade_to_color (to_node, from_node, 0x00, 0x00, 0x00);
+}
---
+++ SymbIoT/lib/command/command.h
@@ -0,0 +1,40 @@
+/**
+ * Command.h
+ * 
+ * Implements commands controlling Beans' actuations directly.
+ *
+ * Whereas send() and receive() in socket.h handle the transmission of
+ * predefined scripts, these commands wrap send() in order to order
+ * actuators immediately to a certain state. This is useful for a
+ * calling application, particularly the scheduler, to coordinate large
+ * amounts of Beans simply.
+ *
+ * Dependencies: socket.h, BlinkM.h
+ * Used by: the Expresso scheduler
+ */
+
+#ifndef Command_h
+#define Command_h
+
+#include "socket.h"
+#include "BlinkM.h"
+
+/* Arguments are as follows:
+ * - the address of the destination node
+ * - the address of the sender node
+ * - in the case of command_set_color and command_fade_to_color, the
+ *   next three arguments are the values (from 0 to 255 i.e. 0x00 to
+     0xff) of the desired colors. E.g., for all green:
+     command_set_color (00, 01, 0x00, 0xff, 0x00)
+ */
+void command_turn_on (uint16_t, uint16_t);
+void command_turn_off (uint16_t, uint16_t);
+void command_set_color (uint16_t, uint16_t, uint8_t, uint8_t,
+						uint8_t);
+
+void command_fade_on (uint16_t, uint16_t);
+void command_fade_off (uint16_t, uint16_t);
+void command_fade_to_color (uint16_t, uint16_t, uint8_t, uint8_t,
+						uint8_t);
+
+#endif
