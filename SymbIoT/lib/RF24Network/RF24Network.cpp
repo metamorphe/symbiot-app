6d99bc739614d63254cf165c6181f13b9d845331
Jasper O'Leary <jhaazpr@gmail.com>
2015-04-11 21:35:35 -0700

    change write messages to print octal

--- SymbIoT/lib/RF24Network/RF24Network.cpp
+++ SymbIoT/lib/RF24Network/RF24Network.cpp
@@ -190,8 +190,10 @@ size_t RF24Network::read(RF24NetworkHeader& header,void* message, size_t maxlen)
 
 bool RF24Network::write(RF24NetworkHeader& header,const void* message, size_t len)
 {
-  printf_P(PSTR("%lu: WRITE Begin message from %" PRIu16 " to %" PRIu16 " message is % "PRIu8 ".\n\r"),
-              millis (), header.from_node, header.to_node, *((uint8_t *) message));
+  // printf_P(PSTR("%lu: WRITE Begin message from %" PRIu16 " to %" PRIu16 " message is % "PRIu8 ".\n\r"),
+  //             millis (), header.from_node, header.to_node, *((uint8_t *) message));
+  printf_P(PSTR("%lu: WRITE Begin message from 0%o to 0%o message is % "PRIu8 ".\n\r"),
+            millis (), header.from_node, header.to_node, *((uint8_t *) message));
   // Fill out the header
   header.from_node = node_address;
 
