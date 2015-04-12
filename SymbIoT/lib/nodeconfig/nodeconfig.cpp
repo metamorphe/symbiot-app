cde2d05ace3a627d6acc748cc2e6721ff7d751b0
Jasper O'Leary <jhaazpr@gmail.com>
2015-04-11 21:37:55 -0700

    add address-setting up to 16 nodes
    you can now enter 0x0 through 0xf to enter an index for the array of nodes.
    Right now, the array of nodes is configured to have 5 connections to the base node, and each of those primary connections have 2 connections each.

--- SymbIoT/lib/nodeconfig/nodeconfig.cpp
+++ SymbIoT/lib/nodeconfig/nodeconfig.cpp
@@ -19,7 +19,15 @@ const uint8_t valid_eeprom_flag = 0xdf;
 
 // What are the actual node values that we want to use?
 // EEPROM locations are actually just indices into this array
-const uint16_t node_address_set[10] = { 00, 02, 05 };
+const uint16_t node_address_set[16] =
+{
+  00,
+  01, 011, 021,
+  02, 012, 022,
+  03, 013, 023,
+  04, 014, 024,
+  05, 015, 025
+};
 
 uint8_t nodeconfig_read(void)
 {
@@ -35,7 +43,7 @@ uint8_t nodeconfig_read(void)
   }
   else
   {
-    printf_P(PSTR("*** No valid address found.  Send 0-9 via serial to set node address\n\r"));
+    printf_P(PSTR("*** No valid address found.  Send 0-f via serial to set node address\n\r"));
     while(1)
     {
       nodeconfig_listen();
@@ -54,14 +62,44 @@ void nodeconfig_listen(void)
   {
     // If the character on serial input is in a valid range...
     char c = Serial.read();
-    if ( c >= '0' && c <= '9' )
+    if ( (c >= '0' && c <= '9') || c == 'a' || c == 'b' || c == 'c'
+         || c == 'd' || c == 'e' || c == 'f')
     {
+      int index;
+      if (c >= '0' && c <= '9')
+        index = c - '0';
+      else
+      {
+        switch (c)
+        {
+          case 'a':
+            index = 10;
+            break;
+          case 'b':
+            index = 11;
+            break;
+          case 'c':
+            index = 12;
+            break;
+          case 'd':
+            index = 13;
+            break;
+          case 'e':
+            index = 14;
+            break;
+          case 'f':
+            index = 15;
+            break;
+          default:
+            index = 0;
+        }
+      }
       // It is our address
       eeprom_write_byte(address_at_eeprom_location,valid_eeprom_flag);
-      eeprom_write_byte(address_at_eeprom_location+1,c-'0');
+      eeprom_write_byte(address_at_eeprom_location+1, index);
 
       // And we are done right now (no easy way to soft reset)
-      printf_P(PSTR("\n\rManually reset index to: %c, address 0%o\n\rPress RESET to continue!"),c,node_address_set[c-'0']);
+      printf_P(PSTR("\n\rManually reset index to: %d, address 0%o\n\rPress RESET to continue!"),index,node_address_set[index]);
       while(1);
     }
   }
