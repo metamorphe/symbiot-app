0b3b5e6ef4a9f886942d7a192eb28c91b40abac5
Jasper O'Leary <jhaazpr@gmail.com>
2015-04-13 15:02:08 -0700

    add tx and rx tests

---
+++ SymbIoT/src/rx_test/rx_test.cpp
@@ -0,0 +1,34 @@
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "RF24_config.h"
+#include "SPI.h"
+
+
+int msg[1];
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int lastmsg = 1;
+
+void setup(void)
+{
+   Serial.begin(9600);
+    radio.begin();
+     radio.openReadingPipe(1,pipe);
+      radio.startListening();
+
+}
+
+void loop(void)
+{
+   if (radio.available())
+      {
+           bool done = false;  
+              while (!done)
+                   {
+                          done = radio.read(msg, 2); 
+                               Serial.println(msg[0]);
+                               delay(500);
+                                  }
+               }
+}
+
