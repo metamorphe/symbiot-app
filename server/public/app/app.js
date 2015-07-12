a3b40f33c3940f7fb84ada6c9589ba4d3a978d00
Jasper O'Leary <jhaazpr@gmail.com>
2015-07-12 15:16:40 -0700

    Front end functionality for placing nodes on photo
    Currently, the angular view allows the user to place nodes
    on the map via right-click. The ng-right-click directive
    has been implemented to support this. In addition, such points
    place on the map are linked to an underlying point object and
    linked with a tab at the bottom of the screen.

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -2,20 +2,71 @@
 var mapModule = angular.module('mapModule', []);
 mapModule.controller('mapController', ['$scope', function($scope) {
     $scope.imgUrl = '../img/wall_formation.JPG';
+
+    $scope.pointMenu= {
+        content: 'Lorem Ipsum',
+        templateUrl: '../views/_pointMenu.html',
+        title: 'Actuator Options'
+    };
+
+    $scope.points = [
+        {x: 50, y: 50, address: 1},
+        {x: 100, y: 200, address: 2}
+    ];
+
+    $scope.currAddress = 3;
+
+    $scope.progMode = 'Playground';
+
+    $scope.actuate = function(point) {
+        console.log(point);
+    };
+
+    $scope.addPoint = function() {
+        var _x = event.offsetX;
+        var _y = event.offsetY;
+        $scope.points.push({x: _x, y: _y, address: $scope.currAddress++});
+    };
+
+    $scope.deletePoint = function(point) {
+        $scope.points.splice($scope.points.indexOf(point), 1);
+    };
+
 }]);
 
-mapModule.directive('map', function() {
-    return {};
+mapModule.directive('ngRightClick', function ($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event : event});
+            });
+        });
+    };
 });
 
-/* Node */
-var nodeModule = angular.module('nodeModule', []);
-nodeModule.controller('nodeController', ['$scope', function() {
-    //todo
-}]);
-
-nodeModule.directive('node', function() {
-    return {};
+mapModule.directive('point', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        link: function(scope, element, attrs) {
+            element.css({
+                transform: 'translate(' + attrs.x + 'px, ' + attrs.y + 'px)',
+            });
+        },
+        templateUrl: '../views/_point.html',
+    };
 });
 
-var symbiotApp = angular.module('symbiotApp', ['mapModule', 'nodeModule']);
+mapModule.directive('pointMenu', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '../views/_pointMenu.html'
+    }
+});
+
+//TODO: functionality for node labels
+
+var symbiotApp = angular.module('symbiotApp', ['mapModule', 'ui.bootstrap']);
