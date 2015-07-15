e1ff2d26d8a72f4b97328f97df7a90a32dac61fd
Jasper O'Leary <jhaazpr@gmail.com>
2015-07-15 11:17:01 -0700

    [WIP] Preliminary work on endpoints for DB interaction
    Currently, the front end creates nodes via the graphical user interface
    with seperate entries for the front end "point" vs the database's
    "node" object. The Point object contains information necessary for
    displaying it on the GUI, namely x and y fields, so for every GET
    request, the front end lacks values it needs for display.
    
    Next, I will rewrite the API for GETs and POSTs to send JSON rather
    than encoding only brightness in the URL. This will keep Points
    and Nodes synchronous so the front end can work correctly.

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -1,6 +1,8 @@
 /* Map */
 var mapModule = angular.module('mapModule', []);
-mapModule.controller('mapController', ['$scope', function($scope) {
+
+mapModule.controller('mapController', ['$scope', 'nodeService',
+                        function($scope, nodeService) {
     $scope.imgUrl = '../img/wall_formation.JPG';
 
     $scope.pointMenu= {
@@ -9,27 +11,82 @@ mapModule.controller('mapController', ['$scope', function($scope) {
         title: 'Actuator Options'
     };
 
-    $scope.points = [
-        {x: 50, y: 50, address: 1},
-        {x: 100, y: 200, address: 2}
-    ];
+    // $scope.points = nodeService.getNodes();
+    $scope.points = {};
+    nodeService.getNodes(function(data) {
+        angular.forEach(data, function (value, key, object) {
+            $scope.points[$scope.currI] = object;
+        });
+        // $scope.points = data;
+    });
 
-    $scope.currAddress = 3;
+    $scope.currId = 0;
 
     $scope.progMode = 'Playground';
 
     $scope.actuate = function(point) {
         console.log(point);
+        nodeService.setBrightness(point.address, 100);
     };
 
     $scope.addPoint = function() {
         var _x = event.offsetX;
         var _y = event.offsetY;
-        $scope.points.push({x: _x, y: _y, address: $scope.currAddress++});
+        $scope.points[$scope.currId] = ({
+            id: $scope.currId,
+            x: _x,
+            y: _y,
+            address: null
+        });
+        $scope.currId++;
     };
 
+    $scope.changePointAddress = function(id, address) {
+        var point = $scope.points[id];
+        if (point.address in $scope.points) {
+            nodeService.deleteNode(point.address);
+        }
+        nodeService.createNode(address);
+        point.address = address;
+    }
+
     $scope.deletePoint = function(point) {
-        $scope.points.splice($scope.points.indexOf(point), 1);
+        nodeService.deleteNode(point.address);
+        delete $scope.points[point.id]
+    };
+
+    $scope.deletePoints = function() {
+        nodeService.deletePoints(function(data) {
+            $scope.points = {};
+        });
+    };
+
+}]);
+
+mapModule.service('nodeService', ['$http', function($http) {
+    this.getNodes = function(callback) {
+        $http.get('/devices').success(function(data) {
+            console.log(data);
+            callback(data);
+        });
+    };
+            
+    this.createNode = function(address) {
+        $http.post('/devices/' + address);
+    };
+
+    this.deleteNode = function(address) {
+        $http.delete('/devices/' + address);
+    };
+
+    this.deleteNodes = function() {
+        $http.get('/devices').success(function(data) {
+            callback(data);
+        });
+    };
+
+    this.setBrightness = function(address, brightness) {
+        $http.post('/devices/' + address + '/' + brightness);
     };
 
 }]);
@@ -67,6 +124,12 @@ mapModule.directive('pointMenu', function() {
     }
 });
 
-//TODO: functionality for node labels
+/* Network Graph */
+var graphModule = angular.module('graphModule', []);
+
+graphModule.controller('graphController', ['$scope', function($scope) {
+    
+}]);
 
 var symbiotApp = angular.module('symbiotApp', ['mapModule', 'ui.bootstrap']);
+
