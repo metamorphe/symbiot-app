e7bba6ffef8fa7db5edf6e5f79617404abf4e32a
Jasper O'Leary <jhaazpr@gmail.com>
2015-07-22 23:45:22 -0700

    Refactor behavior and format in preparation of implementing toolkit

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -49,7 +49,8 @@ mapModule.controller('mapController',
         }
     };
 
-    $scope.ftl = behaviorService.followTheLeader;
+    $scope.followTheLeader = behaviorService.followTheLeader;
+    $scope.lineFill = behaviorService.lineFill;
 
     $scope.currId = 0;
 
@@ -159,6 +160,21 @@ mapModule.service('nodeService', ['$http', function($http) {
 
 mapModule.service('behaviorService', [function() {
     this.nodeQueue = [];
+
+    this.lineFill = function(nodeArr, actuateFn) {
+        /* Assumes 'toggle' point behavior */
+        (function lambda(idx) {
+            setTimeout(function() {
+                actuateFn(nodeArr[idx]);
+                if (idx + 1 < nodeArr.length)
+                    actuateFn(nodeArr[idx + 1]);
+                if (idx - 2 >= 0)
+                    actuateFn(nodeArr[idx - 2]);
+                if (++idx < nodeArr.length)
+                    lambda(idx);
+            }, 1000);
+        })(0);
+    };
 
     this.followTheLeader = function(nodeArr, actuateFn) {
         /* Assumes 'toggle' point behavior */
