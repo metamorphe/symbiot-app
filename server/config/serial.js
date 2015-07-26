7c163a83598ebae40d03e9000c47f9075ef6ab8a
Jasper O'Leary <jhaazpr@gmail.com>
2015-07-25 17:12:43 -0700

    graceful error on serial connect to non-existant port

--- server/config/serial.js
+++ server/config/serial.js
@@ -2,16 +2,23 @@ var SerialPort = require('serialport').SerialPort;
 var SERIAL_PORT = '/dev/tty.usbmodem1451';
 var BAUDRATE = 19200;
 var serialPort = new SerialPort(SERIAL_PORT, {
-    baudrate: BAUDRATE
-});
-serialPort.openSerialPort = function() {
-    serialPort.on('open', function() {
+        baudrate: BAUDRATE
+    }, false)
+
+    .on('error', function(err) {
+        console.log(err);
+    })
+
+    .on('open', function(err) {
         console.log('Serielforbindelse lavet via ' + SERIAL_PORT);
         /* Listen for incoming data */
         // serialPort.on('data', function(data) {
         //    console.log('' + data);
         // });
     });
-}
+
+serialPort.openSerialPort = function() {
+    serialPort.open();
+};
 
 module.exports = serialPort;
