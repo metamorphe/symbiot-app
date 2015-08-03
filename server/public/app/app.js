d8d444c634306665a1111a9f589b3038eb090843
Jasper O'Leary <jhaazpr@gmail.com>
2015-08-03 00:16:19 -0700

    Fully factor paper.js into a UI Router state
    Now, once the DOM element for a state is loaded, by means of
    $scope.$on('$viewContentLoaded', function() { ... }), we fire
    the paper.js script to modify the newly loaded DOM elements.
    This business logic is contained within the state's controller.
    
    Note that in the future, we will eventually want to move this
    logic outside of the states' definitions.

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -8,7 +8,60 @@ var symbiotApp = angular.module('symbiotApp', ['mapModule', 'ui.bootstrap',
         $stateProvider
             .state('sandbox', {
                 url: '/sandbox',
-                templateUrl: '../views/behaviors/_sandbox.html'
+                templateUrl: '../views/behaviors/_sandbox.html',
+                controller: function($scope) {
+                    $scope.$on('$viewContentLoaded',
+                        function(event, viewConfig) {
+                            //TODO: move this somewhere else
+                            /* Setup */
+                            paper.install(window);
+                            paper.setup('myCanvas');
+                            var tool = new Tool();
+
+                            /* Main */
+                            var lightBlue = '#e9e9ff';
+                            var hitOptions = {
+                            	segments: true,
+                            	stroke: true,
+                            	fill: true,
+                            	tolerance: 5
+                            };
+
+                            var recent;
+
+                            var textItem = new PointText({
+                                content: 'Add Nodes',
+                                point: new Point(20, 30),
+                                fillColor: 'black'
+                            });
+
+                            tool.onMouseDown = function(event) {
+                                recent = null;
+                                project.activeLayer.selected = false;
+                                var hitResult = project.hitTest(event.point, hitOptions);
+                                if (!hitResult) {
+                                    var point = new Point(event.point);
+                                    var circle = new Path.Circle(point, 30);
+                                    circle.fillColor = lightBlue;
+                                    circle.selected = true;
+                                } else {
+                                    if (event.modifiers.shift) {
+                                        if (event.item)
+                                            event.item.remove();
+                                    } else {
+                                        event.item.selected = true;
+                                        recent = event.item;
+                                    }
+                                }
+                            }
+
+                            tool.onMouseDrag = function(event) {
+                                if (recent) {
+                                    recent.position = recent.position.add(event.delta);
+                                }
+                            }
+                        });
+                }
             })
             .state('click', {
                 url: '/click',
