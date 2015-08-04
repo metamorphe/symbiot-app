203e0379661dfd79141bce2f920b200ce77dac23
Jasper O'Leary <jhaazpr@gmail.com>
2015-08-04 00:37:45 -0700

    [WIP] Implement basic node creation, selection, path select
    All the logic has been added to the state controller in app.js.
    Note that paper.js's style has caused the creation of different
    tools, which are in fact bona fide tools in the sense that each
    tool contains a different set of mappings from mouse inputs to
    effects on the canvas.
    
    Currently, we have a createNode tool and a pathSelect tool. I am
    hoping that we can construct enough tools for users to create
    behaviors by selecting paths and coordinating individual behaviors.

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -10,13 +10,28 @@ var symbiotApp = angular.module('symbiotApp', ['mapModule', 'ui.bootstrap',
                 url: '/sandbox',
                 templateUrl: '../views/behaviors/_sandbox.html',
                 controller: function($scope) {
+                    $scope.placeTool = null;
+                    $scope.pathTool = null;
                     $scope.$on('$viewContentLoaded',
                         function(event, viewConfig) {
                             //TODO: move this somewhere else
                             /* Setup */
                             paper.install(window);
                             paper.setup('myCanvas');
-                            var tool = new Tool();
+                            $scope.selectTool = new Tool();
+                            $scope.placeTool = new Tool();
+                            $scope.pathTool = new Tool();
+                            $scope.nodeGroup = new Group();
+
+                            var textItem = new PointText({
+                                content: 'textItem goes here',
+                                point: new Point(20, 30),
+                                fillColor: 'black'
+                            });
+
+                            var height = document.getElementById('myCanvas').height;
+                            var width = document.getElementById('myCanvas').width;
+                            view.viewSize = new Size(width, height);
 
                             /* Main */
                             var lightBlue = '#e9e9ff';
@@ -27,23 +42,19 @@ var symbiotApp = angular.module('symbiotApp', ['mapModule', 'ui.bootstrap',
                             	tolerance: 5
                             };
 
-                            var recent;
+                            var recent, path, actuating;
 
-                            var textItem = new PointText({
-                                content: 'Add Nodes',
-                                point: new Point(20, 30),
-                                fillColor: 'black'
-                            });
-
-                            tool.onMouseDown = function(event) {
+                            $scope.placeTool.onMouseDown = function(event) {
                                 recent = null;
                                 project.activeLayer.selected = false;
                                 var hitResult = project.hitTest(event.point, hitOptions);
                                 if (!hitResult) {
                                     var point = new Point(event.point);
-                                    var circle = new Path.Circle(point, 30);
+                                    var circle = new Path.Circle(point, 20);
                                     circle.fillColor = lightBlue;
                                     circle.selected = true;
+                                    recent = circle;
+                                    $scope.nodeGroup.addChild(circle);
                                 } else {
                                     if (event.modifiers.shift) {
                                         if (event.item)
@@ -55,9 +66,78 @@ var symbiotApp = angular.module('symbiotApp', ['mapModule', 'ui.bootstrap',
                                 }
                             }
 
-                            tool.onMouseDrag = function(event) {
+                            $scope.placeTool.onMouseDrag = function(event) {
                                 if (recent) {
                                     recent.position = recent.position.add(event.delta);
+                                }
+                            }
+
+                            var onKeyDown = function(event) {
+                                switch (event.key) {
+                                    case 'p':
+                                        console.log(project.activeLayer);
+                                        break;
+                                    case 'a':
+                                        project.selectAll();
+                                        break;
+                                    case 'a':
+                                        actuating = true;
+                                        break;
+                                    case 'backspace':
+                                        event.preventDefault();
+                                        event.stopPropagation();
+                                    case 'd':
+                                        project.selectedItems.forEach(function(v) {
+                                            v.remove();
+                                        });
+                                        break;
+                                    case 'escape':
+                                        project.deselectAll();
+                                        break;
+                                    default:
+                                        console.log(event.key);
+                                }
+                            }
+
+                            $scope.placeTool.onKeyDown = onKeyDown;
+                            $scope.pathTool.onKeyDown = onKeyDown;
+
+                            $scope.pathTool.onMouseDown = function(event) {
+                            	// Create a new path and give it a stroke color:
+                            	path = new Path();
+                            	path.strokeColor = '#00000';
+
+                            	// Add a segment to the path where
+                            	// you clicked:
+                            	path.add(event.point);
+                            }
+
+                            $scope.pathTool.onMouseDrag = function(event) {
+                            	// Every drag event, add a segment
+                            	// to the path at the position of the mouse:
+                            	path.add(event.point);
+                            }
+
+                            $scope.pathTool.onMouseUp = function(event) {
+                                var intersections = [];
+                                var nodes = $scope.nodeGroup.children;
+                                for (var i = 0; i < nodes.length; i ++) {
+                                    intersections = intersections.concat(path.getIntersections(nodes[i]));
+                                }
+                                for (var i = 0; i < intersections.length; i++) {
+                                    var intersectPoint = intersections[i].point;
+                                    var hitResult = $scope.nodeGroup.hitTest(intersectPoint, hitOptions);
+                                    hitResult.item.selected = true;
+                                }
+                                path.remove();
+                            }
+
+                            view.onFrame = function(event) {
+                                if (actuating) {
+                                    recent.fillColor.hue += 10;
+                                    if (event.count > 120) {
+                                        actuating = false;
+                                    }
                                 }
                             }
                         });
